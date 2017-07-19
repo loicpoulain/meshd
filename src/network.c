8df9204184ccf83f6255f0028c1619c9fdda26fa
Loic Poulain <loic.poulain@gmail.com>
2017-07-19 10:55:39 +0200

    Add network Layer

---
+++ src/network.c
@@ -0,0 +1,343 @@
+/*
+ *
+ * Meshd, Bluetooth mesh stack
+ *
+ * Copyright (C) 2017  Loic Poulain <loic.poulain@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <glib.h>
+#include <errno.h>
+
+#include "utils.h"
+#include "network.h"
+#include "transport.h"
+#include "node.h"
+#include "access.h"
+
+#define MAX_CACHE_ENTRY 100
+
+static GSList *intf_list;
+
+static gint network_msg_compare(gconstpointer a, gconstpointer b)
+{
+	const struct network_msg *nmsga = a, *nmsgb = b;
+
+	if (nmsga->src == nmsgb->src)
+		return memcmp(nmsga->seq, nmsgb->seq, sizeof(nmsga->seq));
+
+	return -1;
+}
+
+struct network_msg *network_msg_alloc(size_t mlen)
+{
+	struct network_msg *nmsg = g_new0(struct network_msg, 1);
+
+	nmsg->ref = 1;
+	nmsg->len = mlen;
+
+	return nmsg;
+}
+
+static void network_relay_routine(work_t *work)
+{
+	struct network *net = container_of(work, struct network, relay_w);
+	struct network_msg *nmsg;
+
+	while ((nmsg = g_queue_pop_head(net->relay_q))) {
+		network_send_msg(net, nmsg);
+		network_msg_unref(nmsg);
+	}
+}
+
+static int network_cache_add(struct network *net, struct network_msg *nmsg)
+{
+	GList *found;
+
+	if (net->cache_q == NULL)
+		net->cache_q = g_queue_new();
+
+	found = g_queue_find_custom(net->cache_q, nmsg, network_msg_compare);
+	if (found) {
+		/* move to head */
+		if (g_queue_peek_head_link(net->cache_q) != found) {
+			g_queue_unlink(net->cache_q, found);
+			g_queue_push_head_link(net->cache_q, found);
+		}
+		return -EALREADY;
+	}
+
+	nmsg = network_msg_clone(nmsg);
+	g_queue_push_head(net->cache_q, nmsg);
+
+	if (g_queue_get_length(net->cache_q) >= MAX_CACHE_ENTRY) {
+		/* TODO Circular buffer */
+		network_msg_unref(g_queue_pop_tail(net->cache_q));
+	}
+
+	return 0;
+}
+
+static int network_msg_obfuscate(struct network *net, struct network_msg *msg,
+				 bool obfuscate)
+{
+	uint8_t priv_counter[16], pecb[16];
+	uint32_t iv = cpu_to_be32(net->iv_index);
+	uint8_t *odata;
+	int err, i;
+
+	/* Privacy Counter = 0x0000000000 || IV Index || Privacy Random */
+	memset(priv_counter, 0, 5);
+	memcpy(&priv_counter[5], &iv, 4);
+	/* Privacy Random = (encDST || encTransportPDU || NetMIC )[0–6]*/
+	memcpy(&priv_counter[9], &msg->dst, 7);
+
+	/* PECB = AES(PrivacyKey, 0x0000000000 || IV Index || Privacy Random) */
+	err = aes_ecb(net->pkey, priv_counter, pecb, true);
+	if (err)
+		return err;
+
+	/* obfuscData = (CTL || TTL || SEQ || SRC) ⊕ PECB[0-5] */
+	odata = ((uint8_t *)&msg + 1); /* ctl is the 2nd octet */
+	for (i = 0; i <= 5; i++)
+		odata[i] = odata[i] ^ pecb[i];
+
+	return 0;
+}
+
+int network_intf_register(struct network_intf *nif)
+{
+	g_message("Network interface %s registered", nif->name);
+
+	intf_list = g_slist_append(intf_list, nif);
+
+	if (nif->open)
+		nif->open(nif);
+
+	return 0;
+}
+
+void network_intf_unregister(struct network_intf *nif)
+{
+	g_message("Network interface %s registered", nif->name);
+
+	intf_list = g_slist_remove(intf_list, nif);
+}
+
+int network_recv_msg(struct network_intf *nif, struct network_msg *nmsg)
+{
+	struct network *net = NULL;
+	GSList *nl;
+	int err;
+
+	for (nl = node.network_l; nl != NULL; nl = nl->next) {
+		struct network *netp = nl->data;
+		struct network_nonce nonce;
+
+		/* msg nid vs network nid (7 lsb) */
+		if (nmsg->nid != (netp->id[7] & 0x7f))
+			continue;
+
+		/* deobfuscate */
+		network_msg_obfuscate(netp, nmsg, false);
+
+		/* Generate network nonce */
+		memset(&nonce, 0, sizeof(nonce));
+		nonce.type = NONCE_NETWORK;
+		memcpy(&nonce.ctl_ttl, (void *)nmsg + 1, 6);
+		nonce.iv_index = cpu_to_be32(netp->iv_index);
+
+		/* Authenticate and in-place decrypt of dst + transport pdu */
+		err = aes_ccm(netp->key, (struct nonce *)&nonce,
+			      (uint8_t *)&nmsg->dst, sizeof(nmsg->dst) +
+			      NMSG_PDU_SZ(nmsg) + NMSG_MIC_SZ(nmsg),
+			      (uint8_t *)&nmsg->dst, NMSG_MIC_SZ(nmsg), false);
+		if (!err) {
+			/* msg authenticated and decrypted */
+			net = netp;
+			break;
+		}
+
+		/* need to re-obfuscate the msg, unlikely... */
+		network_msg_obfuscate(netp, nmsg, true);
+	}
+
+	if (!net) /* discard */
+		return -EINVAL;
+
+	/* Check and add to cache */
+	err = network_cache_add(net, nmsg);
+	if (err)
+		return err;
+
+	/* Forward to low transport layer */
+	transport_low_recv(net, nmsg);
+
+	/* Relay ? */
+	if (nmsg->ttl < 2)
+		return 0;
+
+	nmsg->ttl--;
+
+	network_msg_ref(nmsg);
+	g_queue_push_tail(net->relay_q, nmsg);
+
+	/* It is recommended that a small random delay is introduced
+	 * between receiving a Network PDU and relaying a Network PDU
+	 * to avoid collisions between multiple relays that have received
+	 * the Network PDU at the same time.
+	 */
+	schedule_delayed_work(&net->relay_w, rand() % 100);
+
+	return 0;
+}
+
+int network_send_msg(struct network *net, struct network_msg *nmsg)
+{
+	struct network_nonce nonce;
+	GSList *intfl;
+	int err;
+
+	/* least significant bit of the current IV */
+	nmsg->ivi = net->iv_index & 0x01;
+
+	/* 7 least significant bits of the network key identifier */
+	nmsg->nid = net->id[7] & 0x7f;
+
+	/* Generate network nonce */
+	memset(&nonce, 0, sizeof(nonce));
+	nonce.type = NONCE_NETWORK;
+	memcpy(&nonce.ctl_ttl, (void *)nmsg + 1, 6);
+	nonce.iv_index = cpu_to_be32(net->iv_index);
+
+	/* Authenticate and in-place encrypt of dst + transport pdu */
+	err = aes_ccm(net->key, (struct nonce *)&nonce, (uint8_t *)&nmsg->dst,
+		      sizeof(nmsg->dst) + NMSG_PDU_SZ(nmsg) + NMSG_MIC_SZ(nmsg),
+		      (uint8_t *)&nmsg->dst, NMSG_MIC_SZ(nmsg), true);
+	if (err) {
+		g_error("Unable to encrypt network msg");
+		return -EINVAL;
+	}
+
+	if (network_msg_obfuscate(net, nmsg, true)) {
+		g_error("Unable to obfuscate network msg");
+		return -EINVAL;
+	}
+
+	for (intfl = intf_list; intfl != NULL; intfl = intfl->next) {
+		struct network_intf *nif = intfl->data;
+
+		nif->sendmsg(nif, nmsg);
+	}
+
+	return 0;
+}
+
+struct network *network_provision(uint8_t net_key[16], uint16_t key_index,
+	 			  uint32_t iv_index, uint16_t addr)
+{
+	struct network *net;
+
+//	if (network_by_index(key_index))
+//		return NULL;
+
+	net = g_new0(struct network, 1);
+
+	net->index = key_index;
+	memcpy(net->key, net_key, sizeof(net->key));
+	net->iv_index = iv_index;
+	net->addr = addr;
+
+	/* generate nid */
+	k3(net_key, net->id);
+
+	g_message("Network provisioned (NID = %02x; addr = %04x)",
+		  net->id[7] & 0x7f, net->addr);
+
+	init_work(&net->relay_w, network_relay_routine);
+	net->relay_q = g_queue_new();
+
+	node.network_l = g_slist_append(node.network_l, net);
+
+	return net;
+}
+
+struct network *network_provision_new(void)
+{
+	struct network *net;
+	uint8_t netkey[16];
+	int i;
+
+	GRand *rand = g_rand_new();
+
+	/* Generate random network key */
+	for (i = 0; i < (sizeof(netkey) / sizeof(guint32)); i++) {
+		((guint32 *)&netkey)[i] = g_rand_int(rand);
+	}
+
+	net = network_provision(netkey, 0, 0, g_rand_int(rand));
+
+	g_rand_free(rand);
+
+	return net;
+}
+
+static void release_network(struct network *net)
+{
+	g_queue_free_full (net->relay_q, (GDestroyNotify)network_msg_unref);
+	g_queue_free_full (net->cache_q, (GDestroyNotify)network_msg_unref);
+	g_free(net);
+}
+
+/* Local Interface */
+static GQueue local_q = G_QUEUE_INIT;
+static void local_dequeue(work_t *work);
+static work_t local_w = INIT_WORK(local_dequeue);
+
+static int local_sendmsg(struct network_intf *nif, struct network_msg *nmsg)
+{
+	network_msg_ref(nmsg);
+	g_queue_push_tail(&local_q, nmsg);
+	schedule_work(&local_w);
+	return 0;
+}
+
+static struct network_intf local_intf = {
+	.name = "local",
+	.type = NET_INTF_LOCAL,
+	.sendmsg = local_sendmsg,
+};
+
+static void local_dequeue(work_t *work)
+{
+	struct network_msg *nmsg;
+
+	while ((nmsg = g_queue_pop_head(&local_q))) {
+		network_recv_msg(&local_intf, nmsg);
+		network_msg_unref(nmsg);
+	}
+}
+/* */
+
+int network_layer_init(void)
+{
+	//network_intf_register(&local_intf);
+	return 0;
+}
+
+void network_layer_cleanup(void)
+{
+	g_slist_free_full(node.network_l, (GDestroyNotify)release_network);
+}
---
+++ src/network.h
@@ -0,0 +1,166 @@
+/*
+ *
+ * Meshd, Bluetooth mesh stack
+ *
+ * Copyright (C) 2017  Loic Poulain <loic.poulain@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __NETWORK_H
+#define __NETWORK_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <glib.h>
+
+#include "utils.h"
+#include "workqueue.h"
+#include "node.h"
+
+/**
+ * struct network_msg - network message (29-octet max)
+ * @ivi:	[1-bit] Least significant bit of IV Index
+ * @nid:	[7-bit] Value derived from the NetKey used to identify the
+ * 		Encryption Key and Privacy Key used to secure this PDU
+ * @ctl:	[1-bit] Network Control
+ * @ttl:	[7-bit] Time To Live
+ * @seq:	[24-bit] Sequence Number
+ * @src:	[16-bit] Source Address
+ * @dst:	[7-bit] Destination Address
+ * @pdu_mic:	[(8 to 128)-bit] Transport Payload | [(32 or 64)-bit] MIC
+ *
+ * @len:	size of network PDU (hdr + transport pdu + mic)
+ * @ref:	reference counter
+ * @net:	Network associated to this msg
+ *
+ * All multiple-octet values are big endian/network byte order
+ */
+struct network_msg {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	uint8_t nid:7;		/* octet 0 */
+	uint8_t ivi:1;		/* octet 0 */
+	uint8_t ttl:7;		/* octet 1 */
+	uint8_t ctl:1;		/* octet 1 */
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	uint8_t ivi:1;		/* octet 0 */
+	uint8_t nid:7;		/* octet 0 */
+	uint8_t ctl:1;		/* octet 1 */
+	uint8_t ttl:7;		/* octet 1 */
+#endif
+	uint8_t seq[3];		/* octet 2, 3 & 4 */
+	uint16_t src;		/* octet 5 & 6 */
+	uint16_t dst;		/* octet 7 & 8 */
+	uint8_t pdu_mic[20];	/* octet 9 to 28 */
+
+	/* Meta Data */
+	size_t len;
+	int ref;
+} __attribute__ ((packed));
+
+#define NMSG_HDR_SZ(nmsg) (9)
+#define NMSG_MIC_SZ(nmsg) ((nmsg)->ctl ? 8 : 4)
+#define NMSG_PDU_SZ(nmsg) ((nmsg)->len - NMSG_HDR_SZ(nmsg) - NMSG_MIC_SZ(nmsg))
+
+typedef enum {
+	NET_INTF_LOCAL,
+	NET_INTF_ADV,
+	NET_INTF_GATT,
+	NET_INTF_MAX
+} network_intf_type_t;
+
+struct network_intf;
+
+typedef int (*network_intf_sendmsg_func_t) (struct network_intf *nif,
+					    struct network_msg *msg);
+typedef int (*network_intf_open_func_t) (struct network_intf *nif);
+typedef void (*network_intf_close_func_t) (struct network_intf *nif);
+
+struct network_intf {
+	char name[8];
+	network_intf_type_t type;
+	network_intf_open_func_t open;
+	network_intf_close_func_t close;
+	network_intf_sendmsg_func_t sendmsg;
+};
+
+struct network {
+	uint8_t index;
+	GQueue *cache_q;
+	uint8_t key[16];
+	uint8_t pkey[16];
+	uint8_t id[8];
+	uint32_t iv_index;
+	uint16_t addr;
+	uint32_t shared_seq:24;
+	work_t relay_w;
+	GQueue *relay_q;
+	bool relay;
+	void *trans_priv;
+};
+
+struct network_msg *network_msg_alloc(size_t mlen);
+
+static inline uint32_t network_peek_seq(struct network *net)
+{
+	return net->shared_seq++;
+}
+
+static inline struct network_msg *network_msg_ref(struct network_msg *nmsg)
+{
+	nmsg->ref++;
+
+	return nmsg;
+}
+
+static inline void network_msg_unref(struct network_msg *nmsg)
+{
+	if (--nmsg->ref <= 0)
+		g_free(nmsg);
+}
+
+static inline struct network_msg *network_msg_clone(struct network_msg *msg)
+{
+	struct network_msg *msg_clone = network_msg_alloc(msg->len);
+
+	memcpy(msg_clone, msg, msg->len);
+
+	return msg_clone;
+}
+
+static inline struct network *network_by_index(int index)
+{
+	GSList *l;
+
+	for (l = node.network_l; l != NULL; l = l->next) {
+		struct network *net = l->data;
+
+		if (net->index == index)
+			return net;
+	}
+
+	return NULL;
+}
+
+void network_layer_cleanup(void);
+int network_intf_register(struct network_intf *nif);
+void network_intf_unregister(struct network_intf *nif);
+int network_recv_msg(struct network_intf *nif, struct network_msg *nmsg);
+int network_send_msg(struct network *net, struct network_msg *nmsg);
+struct network *network_provision(uint8_t net_key[16], uint16_t key_index,
+	 			  uint32_t iv_index, uint16_t addr);
+struct network *network_provision_new(void);
+
+#endif
